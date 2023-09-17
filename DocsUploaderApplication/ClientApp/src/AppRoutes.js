c1250ccc0438e0e4f727a9bbc22b406b5ead4868
Yuliia Pochtarova <yuliia.pochtarova@hneu.net>
2023-09-17 21:20:31 +0400

    Add connection string stuff

--- DocsUploaderApplication/ClientApp/src/AppRoutes.js
+++ DocsUploaderApplication/ClientApp/src/AppRoutes.js
@@ -1,5 +1,4 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
+
 import { Home } from "./components/Home";
 
 const AppRoutes = [
@@ -7,14 +6,6 @@ const AppRoutes = [
     index: true,
     element: <Home />
   },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
 ];
 
 export default AppRoutes;
