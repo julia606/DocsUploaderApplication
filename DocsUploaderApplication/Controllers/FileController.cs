bf1e5608d03a35760833241ca6df2f3b1c103194
Yuliia Pochtarova <yuliia.pochtarova@hneu.net>
2023-09-17 16:21:22 +0400

    Add logic to file upload

--- DocsUploaderApplication/Controllers/FileController.cs
+++ DocsUploaderApplication/Controllers/FileController.cs
@@ -3,12 +3,46 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DocsUploaderApplication.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class FileController : Controller
     {
         private readonly BlobServiceClient _blobServiceClient;
-        public IActionResult Index()
+        public FileController(BlobServiceClient blobServiceClient)
         {
-            return View();
+            _blobServiceClient = blobServiceClient;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload()
+        {
+            var file = Request.Form.Files[0];
+            var email = Request.Form["email"].ToString();
+
+            if (FileValidationService.IsFileSelected(file))
+            {
+                if (FileValidationService.IsFileValid(file, 10485760) && EmailValidationService.IsEmailValid(email))
+                {
+                    var containerClient = _blobServiceClient.GetBlobContainerClient("blobcontainer");
+                    var blobClient = containerClient.GetBlobClient(file.FileName);
+
+                    using (var stream = file.OpenReadStream())
+                    {
+                        blobClient.SetMetadata(new Dictionary<string, string>
+                        {
+                            { "Email", email }
+                        });
+                        await blobClient.UploadAsync(stream, true);
+                    }
+
+                    return Ok($"File uploaded successfully.");
+                }
+                return BadRequest("Invalid data");
+            }
+            else
+            {
+                return BadRequest("No file selected.");
+            }
         }
     }
 }
